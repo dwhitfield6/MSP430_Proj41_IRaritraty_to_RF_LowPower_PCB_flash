ab426e5237ff5801a7a4a9bca2bf42277411e053
dwhitfield6 <psk.kd.whitfield@gmail.com>
2015-02-20 16:10:48 -0800

    Create.

---
+++ Debug/functions.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+functions.obj: ../functions.c
+functions.obj: ../functions.h
+functions.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+functions.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430g2231.h
+functions.obj: c:/ti/ccsv6/ccs_base/msp430/include/in430.h
+functions.obj: c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/intrinsics.h
+
+../functions.c: 
+../functions.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/msp430g2231.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/intrinsics.h: 
